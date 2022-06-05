122a3da96d52ad4b587c3387b6fa21e688ffce37
1Regina <1reginacheong@gmail.com>
2022-06-06 02:36:01 +0800

    feat: signup and signin response

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,21 +1,39 @@
 import { API } from "../config";
 
-const signup = async (user) => {
-    // user can give name, email, password
-    // console.log(name, email, password);
-   try {
-      const response = await fetch(`${API}/signup`, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user), // to convert object to json string for backend
-      });
-      return await response.json();
-    } catch (err) {
-      console.log(err);
-    }
-  };
+export const signup = async (user) => {
+  // user can give name, email, password
+  // console.log(name, email, password);
+  try {
+    const response = await fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user), // to convert object to json string for backend
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-  export default signup
+export const signin = async (user) => {
+  // user can give name, email, password
+  // console.log(name, email, password);
+  try {
+    const response = await fetch(`${API}/signin`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user), // to convert object to json string for backend
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
