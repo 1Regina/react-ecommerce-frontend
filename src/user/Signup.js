53a2edf7129f5b4fa672a8b7464d0fd17efc9efb
1Regina <1reginacheong@gmail.com>
2022-06-06 01:11:07 +0800

    feat: signup frontend components and refactor

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,10 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Layout from "../core/Layout";
-import {API} from "../config"
+import signup from "../auth/index"
 
-const Signup = () => (
-  <Layout title="Signup" description="Signup to Node React E-commerce App">
- ...
-  </Layout>
-);
+const Signup = () => {
+  // inefficient to do useState per field
+  // const [name, setName] = useState('')
+  // const [email, setEmail] = useState('')
+  // const [password, setPassword] = useState('')
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "", // when create new user and there is error, this field will populate
+    success: false,
+  });
+
+  // grab fields input from state
+  const { name, email, password, success, error } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value }); // this name is dynamic n could refer to email / password too
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault(); // so the browser wont reload when button is clicked
+    setValues({...values, error:false})
+    // signup({name: name, email: email, password: password});
+    // can be simplify as below as the key and values are the same
+    signup({ name, email, password }) // to send the javascript object as user thru the signup
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true,
+          });
+        }
+      });
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control"
+          value={name}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className="alert alert-info"
+      style={{ display: success ? "" : "none" }}
+    >
+      New account is created. Please <Link to="/signin">Signin</Link> 
+    </div>
+  );
+
+  return (
+    <Layout
+      title="Signup"
+      description="Signup to Node React E-commerce App"
+      className="container col-md8 offset-md-2"
+    >
+      {showSuccess()}
+      {showError()}
+      {signUpForm()}
+      {/* {JSON.stringify(values)} */}
+    </Layout>
+  );
+};
 export default Signup;
